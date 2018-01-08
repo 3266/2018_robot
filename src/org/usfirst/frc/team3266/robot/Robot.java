f2ce17e006e6b0c05cb26a939cd50a1208e7ae9f
3266 <35179064+3266@users.noreply.github.com>
2018-01-08 15:38:53 -0500

    Add files via upload

--- src/org/usfirst/frc/team3266/robot/Robot.java
+++ src/org/usfirst/frc/team3266/robot/Robot.java
@@ -1,21 +1,29 @@
 package org.usfirst.frc.team3266.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends SampleRobot {
-	RobotDrive robotDrive = new RobotDrive(0,1,2,3);
 	
-	Talon lr = new Talon(5);
-	Talon rr = new Talon(4);
-	
-	Joystick stick = new Joystick(0);
+	RobotDrive driveTrain;
+	Talon leftArm;
+	Talon rightArm;
+	Talon elevator;
+	Joystick controller;
 
 	public Robot() {
-		robotDrive.setExpiration(0.1);
+		
+		controller = new Joystick(0);
+		
+		driveTrain = new RobotDrive(0,1,2,3);
+		rightArm = new Talon(4);
+		leftArm = new Talon(5);
+		elevator = new Talon(6);
+		
+		driveTrain.setExpiration(0.1);
 	}
 
 	@Override
@@ -28,34 +36,37 @@ public class Robot extends SampleRobot {
 		
 	}
 
-	/**
-	 * Runs the motors with arcade steering.
-	 */
 	@Override
 	public void operatorControl() {
-		robotDrive.setSafetyEnabled(true);
+		
+		driveTrain.setSafetyEnabled(true);
+		
 		while (isOperatorControl() && isEnabled()) {
 			
-			if (stick.getRawButton(1)) {
-				lr.set(-1.0);
-				rr.set(1.0);
-			} else if (stick.getRawButton(2)) {
-				lr.set(1.0);
-				rr.set(-1.0);
+			if (controller.getRawButton(1)) {
+				// A - intakes power cubes
+				leftArm.set((-1.0)*0.66);
+				rightArm.set((1.0)*0.66);
+			
+
+			} else if (controller.getRawButton(2)) {
+				// B - spits out power cubes
+				leftArm.set(1.0);
+				rightArm.set(-1.0);
+				
 			} else {
-				lr.set(0);
-				rr.set(0);
+				// stop the motors if no input is received
+				leftArm.set(0);
+				rightArm.set(0);
+				
 			}
 			
-			robotDrive.arcadeDrive(-stick.getRawAxis(1),-stick.getRawAxis(0));
+			driveTrain.arcadeDrive((-controller.getRawAxis(1))*0.77,(-controller.getRawAxis(0))*0.77);
 			
 			Timer.delay(0.005); // wait for a motor update time
 		}
 	}
 
-	/**
-	 * Runs during test mode
-	 */
 	@Override
 	public void test() {
 	}
